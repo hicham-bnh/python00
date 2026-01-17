589edf2666aa3a1758cb8bef1079da3a329e1c02
Mohammed Hicham Benhabib <mobenhab@z4r9p2.42lyon.fr>
2026-01-17 12:47:53 +0100

    i delet the -> None for the ex7

--- ex7/ft_seed_inventory.py
+++ ex7/ft_seed_inventory.py
@@ -1,4 +1,4 @@
-def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
+def ft_seed_inventory(seed_type: str, quantity: int, unit: str):
     if (seed_type == "packets"):
         print(f"{seed_type} seeds: {quantity} {unit} avaible")
     elif (unit == "grams"):
