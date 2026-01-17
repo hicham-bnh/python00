e39ce254b0036319c0a4e830a686d43ed68a83c5
Mohammed Hicham Benhabib <mobenhab@z4r9p2.42lyon.fr>
2026-01-17 12:45:41 +0100

    project finish

--- ex7/ft_seed_inventory.py
+++ ex7/ft_seed_inventory.py
@@ -2,7 +2,7 @@ def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     if (seed_type == "packets"):
         print(f"{seed_type} seeds: {quantity} {unit} avaible")
     elif (unit == "grams"):
-        print(f"{seed_type} seeds: {quantity} {unit} avaible") 
+        print(f"{seed_type} seeds: {quantity} {unit} avaible")
     elif (unit == "area"):
         print(f"{seed_type} seeds: quantity {quantity} meters")
     else:
